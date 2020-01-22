5f276fc345e68c7f434f63cabc2a3a4de17376de
Jairo Collao <illapu1xyz@gmail.com>
2020-01-22 02:42:19 -0500

    Algunos añadiduras hechas

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 /* eslint-disable prettier/prettier */
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentWillMount(){
+    firebase.initializeApp({
+      apiKey: 'AIzaSyD0yr73KFFjSGZcYWxyB965-N3ULUaOIew',
+      authDomain: 'authentication-cb581.firebaseapp.com',
+      databaseURL: 'https://authentication-cb581.firebaseio.com',
+      projectId: 'authentication-cb581',
+      storageBucket: '',
+      messagingSenderId: '547554416899',
+      appId: '1:547554416899:web:ed7d3b9838636a25',
+    });
+  }
+
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <Text>An app</Text>
+        <LoginForm />
       </View>
     );
   }
---
+++ src/components/LoginForm.js
@@ -0,0 +1,22 @@
+/* eslint-disable prettier/prettier */
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import { Button, Card, CardSection } from './common';
+
+class LoginForm extends Component {
+  render() {
+    return (
+      <Card>
+        <CardSection />
+        <CardSection />
+        <CardSection>
+          <Button>
+            Log In
+          </Button>
+        </CardSection>
+      </Card>
+    );
+  }
+}
+
+export default LoginForm;

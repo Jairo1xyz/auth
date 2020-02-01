84733b1908aac288a09c0fa208825e753dd2e470
Jairo Collao <illapu1xyz@gmail.com>
2020-01-31 23:52:08 -0500

    Se ha realizado el proceso de Login y Logout teniendo en cuenta la  experiencia de usuario en la interacción con la app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 /* eslint-disable prettier/prettier */
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner, CardSection } from './components/common';
 import LoginForm from './components/LoginForm';
-import { API_KEY, AUTH_DOMAIN, DATABASE_URL, PROJECT_ID, 
-  STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID }
+import { API_KEY, AUTH_DOMAIN, DATABASE_URL, PROJECT_ID,
+  STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID, MEASUREMENT_ID }
   from '../my_firebase_keys/firebase_keys';
 
 class App extends Component {
+  state = { loggedIn: null };
+
   componentDidMount(){
     firebase.initializeApp({
       apiKey: API_KEY,
@@ -18,14 +20,42 @@ class App extends Component {
       storageBucket: STORAGE_BUCKET,
       messagingSenderId: MESSAGING_SENDER_ID,
       appId: APP_ID,
+      measurementId: MEASUREMENT_ID,
     });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent(){
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <CardSection>
+            <Button onPress={() => firebase.auth().signOut()}>
+              Log Out
+            </Button>
+          </CardSection>);
+      case false:
+        return <LoginForm/>;
+      default:
+        return (
+          <CardSection>
+            <Spinner size="large"/>
+          </CardSection>);
+    }
   }
 
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
